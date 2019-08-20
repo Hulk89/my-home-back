4bda84461183792f4619e8f73f485be53db15e06
Hulk.Oh <snuboy89@gmail.com>
2019-08-20 10:43:07 +0900

    app 만들고 serve하도록 수정

--- app.py
+++ app.py
@@ -11,42 +11,53 @@ from models import models
 from routes.verify import token_required, encode_jwt
 from routes.comics import comics
 
-app = Flask(__name__)
+def make_routes(data_path, config):
+    app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+    app.config.from_object(Config)
+    app.config["data_folder_path"] = data_path
+    CORS(app)
+    with open(config) as f:
+        db_config = json.loads(f.read())
+    # sqlalchemy 세팅
+    db.initialize(**db_config)
 
-@app.route('/login', methods=('POST',))
-def login():
-    data = request.get_json()
-    if models.User.check_password(db.SESSION,
-                                  data['user'],
-                                  data['password']):
-        print("login success")
-        token = encode_jwt(data['user'])
-        return jsonify({'token': token.decode('UTF-8')})
-    else:
-        return jsonify({'message': 'Invalid credentials',
-                        'authenticated': False}), 401
-@app.route("/select", methods=('GET', ))
-@token_required
-def select():
-    print(db.SESSION.query(models.Todo).all())
-    return str(db.SESSION.query(models.Todo).all())
+    @app.route("/")
+    def hello():
+        return "Hello World!"
 
-@app.route("/put", methods=('POST', ))
-@token_required
-def put():
-    data = request.get_json()
-    db.SESSION.add(models.Todo(data["title"],
-                               data["description"],
-                               models.TodoState.todo))
-    db.SESSION.commit()
-    return "Good"
+    @app.route('/login', methods=('POST',))
+    def login():
+        data = request.get_json()
+        if models.User.check_password(db.SESSION,
+                                    data['user'],
+                                    data['password']):
+            print("login success")
+            token = encode_jwt(data['user'])
+            return jsonify({'token': token.decode('UTF-8')})
+        else:
+            return jsonify({'message': 'Invalid credentials',
+                            'authenticated': False}), 401
+    @app.route("/select", methods=('GET', ))
+    @token_required
+    def select():
+        print(db.SESSION.query(models.Todo).all())
+        return str(db.SESSION.query(models.Todo).all())
 
+    @app.route("/put", methods=('POST', ))
+    @token_required
+    def put():
+        data = request.get_json()
+        db.SESSION.add(models.Todo(data["title"],
+                                data["description"],
+                                models.TodoState.todo))
+        db.SESSION.commit()
+        return "Good"
 
-app.register_blueprint(comics, url_prefix='/comics')
+    app.register_blueprint(comics, url_prefix='/comics')
+
+    return app
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="my-home-backend")
@@ -54,13 +65,6 @@ if __name__ == "__main__":
     parser.add_argument("--folder_path", required=True)
     args = parser.parse_args()
 
-    app.config.from_object(Config)
-    app.config["data_folder_path"] = args.folder_path
-    CORS(app)
-    with open(args.config) as f:
-        db_config = json.loads(f.read())
-    # sqlalchemy 세팅
-    db.initialize(**db_config)
-
+    app = make_routes(args.folder_path, args.config)
     app.run()
 
